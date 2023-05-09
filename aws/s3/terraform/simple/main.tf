7a94b58ee4e21a51e3472eb797e623bf4497a669
UltTransformer <chris.g.noon@gmail.com>
2023-05-09 15:34:37 +0100

    adding base ec2 config

--- aws/s3/terraform/simple/main.tf
+++ aws/s3/terraform/simple/main.tf
@@ -20,14 +20,9 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_acl" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_versioning" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
   versioning_configuration {
-    status = "Enabled"
+    status = "False"
   }
 }
