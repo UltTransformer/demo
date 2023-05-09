a198de6491d52b75aae2b5596dcec7813d2984f6
UltTransformer <chris.g.noon@gmail.com>
2023-05-09 15:16:18 +0100

    adding base vpc config

---
+++ aws/vpc/terraform/simple/outputs.tf
@@ -0,0 +1,14 @@
+# Defining outputs
+
+output "vpc_1" {
+  value = aws_vpc.vpc_1.id
+}
+# output "subnet_1" {
+#   value = aws_subnet.subnet_1.id 
+# }
+# output "subnet_2" {
+#   value = aws_subnet.subnet_2.id
+# }
+# output "subnet_3" {
+#   value = aws_subnet.subnet_3.id
+# }
