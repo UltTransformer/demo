dbf641faa9af19c01d160cc7a733a4d2784f3da9
UltTransformer <chris.g.noon@gmail.com>
2023-05-09 15:47:41 +0100

    adding base ec2 config

---
+++ aws/ec2/terraform/simple/inputs.tf
@@ -0,0 +1,12 @@
+# env0 remote backend
+
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+  config = {
+    hostname     = "backend.api.env0.com"
+    organization = "a25b6a59-9c48-4917-b082-68e985319a87"
+    workspaces = {
+      name = "demo-vpc"
+    }
+  }
+}
