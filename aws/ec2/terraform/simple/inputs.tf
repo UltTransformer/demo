cbddcf8e7091773e63439e64e8a5058aa3b319fd
UltTransformer <chris.g.noon@gmail.com>
2023-05-09 16:22:25 +0100

    adding base ec2 config

--- aws/ec2/terraform/simple/inputs.tf
+++ aws/ec2/terraform/simple/inputs.tf
@@ -17,13 +17,9 @@ data "aws_vpc" "vpc_1" {
 data "aws_subnet" "subnet_1" {
   id = data.terraform_remote_state.vpc.outputs.subnet_1
 }
-# data "aws_subnet" "subnet_2" {
-#   id = data.terraform_remote_state.vpc.outputs.subnet_2.id
-# }
-# data "aws_subnet" "subnet_3" {
-#   id = data.terraform_remote_state.vpc.outputs.subnet_3.id
-# }
-
-# output "vpc_id" {
-#   value = data.terraform_remote_state.vpc.outputs.vpc_id
-# }
+data "aws_subnet" "subnet_2" {
+  id = data.terraform_remote_state.vpc.outputs.subnet_2
+}
+data "aws_subnet" "subnet_3" {
+  id = data.terraform_remote_state.vpc.outputs.subnet_3
+}
