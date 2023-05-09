6a985e82294b791203485e98c2459007d6fecbc9
UltTransformer <chris.g.noon@gmail.com>
2023-05-09 15:26:18 +0100

    adding base ec2 config

--- aws/ec2/terraform/simple/variables.tf
+++ aws/ec2/terraform/simple/variables.tf
@@ -1,18 +1,23 @@
 # Variables file
 
-variable "ec2_instance_size" {
-  type    = string
-  default = "t3.micro"
-}
-
-variable "ec2_name" {
-  type    = string
-  default = "change-me"
-}
-
 variable "region" {
   type    = string
   default = "eu-north-1a"
+}
+
+variable "ec2-instance-name" {
+  type = string
+  default = "CHANGE ME"
+}
+
+variable "security-group-name" {
+  type = string
+  default = "CHANGE ME"
+}
+
+variable "ec2_instance_size" {
+  type    = string
+  default = "t3.micro"
 }
 
 variable "ingress_sg_cidr" {
@@ -23,12 +28,4 @@ variable "ingress_sg_cidr" {
 variable "egress_sg_cidr" {
   type    = string
   default = "10.10.0.0/16"
-}
-
-variable "security-group-name" {
-  type = string
-}
-
-variable "ec2-instance-name" {
-  type = string
 }
