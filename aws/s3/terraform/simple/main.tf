a5de580ca677588c855ed613c0a4cf241ab5f175
UltTransformer <chris.g.noon@gmail.com>
2023-05-09 15:30:59 +0100

    adding base ec2 config

---
+++ aws/s3/terraform/simple/main.tf
@@ -0,0 +1,33 @@
+# Provider
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+# Bucket
+
+# Create s3 bucket
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
---
+++ aws/s3/terraform/simple/variables.tf
@@ -0,0 +1,7 @@
+# Input variable definitions
+
+variable "bucket_name" {
+  description = "name of the s3 bucket. Must be unique."
+  type        = string
+  default     = "CHANGE ME"
+}
