40da7276ccc6dd0bb76994042440d1da5c0e2c72
UltTransformer <chris.g.noon@gmail.com>
2023-05-09 15:05:29 +0100

    adding base vpc config

--- aws/vpc/terraform/simple/vars.tf
+++ aws/vpc/terraform/simple/vars.tf
@@ -2,47 +2,47 @@
 
 # Vatiable definitions 
 
-variable "vpc_1_cidr_1_local" {
+variable "vpc_1_cidr_1" {
   type    = string
   default = "10.0.0.0/16"
 }
-variable "subnet_1_local" {
+variable "subnet_1" {
   type    = string
   default = "10.0.101.0/24"
 }
-variable "subnet_2_local" {
+variable "subnet_2" {
   type    = string
   default = "10.0.102.0/24"
 }
-variable "subnet_3_local" {
+variable "subnet_3" {
   type    = string
   default = "10.0.103.0/24"
 }
-variable "subnet_public_local_1" {
+variable "subnet_public" {
   type    = string
   default = "10.0.201.0/24"
 }
-variable "subnet_public_local_2" {
+variable "subnet_public" {
   type    = string
   default = "10.0.202.0/24"
 }
-variable "subnet_public_local_3" {
+variable "subnet_public" {
   type    = string
   default = "10.0.203.0/24"
 }
-variable "region_local" {
+variable "region" {
   type    = string
   default = "eu-north-1"
 }
-variable "sub_region_1_local" {
+variable "sub_region_1" {
   type    = string
   default = "eu-north-1a"
 }
-variable "sub_region_2_local" {
+variable "sub_region_2" {
   type    = string
   default = "eu-north-1b"
 }
-variable "sub_region_3_local" {
+variable "sub_region_3" {
   type    = string
   default = "eu-north-1c"
 }
