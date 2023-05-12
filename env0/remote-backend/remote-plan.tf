5947cbfe203a9a7da4b8e368bbcf38512686cfc3
UltTransformer <chris.g.noon@gmail.com>
2023-05-12 18:45:54 +0200

    adding remote backend base

--- env0/remote-backend/remote-plan.tf
+++ env0/remote-backend/remote-plan.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
     hostname     = "backend.api.env0.com"
-    # organization = "[Org ID].[Project ID]"
+    organization = "[Org ID].[Project ID]"
     workspaces {
-      name = "c39c934bcx02nx0n2e"
+      name = "[workspaceName]"
     }
   }
 }
