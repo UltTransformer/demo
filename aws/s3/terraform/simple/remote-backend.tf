b847d5db74a6abcc6011a26bd9302abe0d3eb30a
UltTransformer <chris.g.noon@gmail.com>
2023-05-13 19:12:38 +0200

    adding s3 remote backend

--- aws/s3/terraform/simple/remote-backend.tf
+++ aws/s3/terraform/simple/remote-backend.tf
@@ -3,7 +3,17 @@
 terraform {
   backend "s3" {
     bucket = "cn-env0-statefile-bucket"
-    key    = "vpc.tfstate"
+    key    = "s3.tfstate"
     region = "eu-north-1"
   }
 }
+
+# terraform {
+#   backend "remote" {
+#     hostname = "backend.api.env0.com"
+#     organization = "a25b6a59-9c48-4917-b082-68e985319a87.d9a947b9-2e16-4004-a5f2-7d2f4b686444"
+#     workspaces {
+#       name = "demo-s3"
+#     }
+#   }
+# }
