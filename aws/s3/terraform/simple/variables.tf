0a0ca95109e838c19d1fad5bb9d89b7dff807dcb
UltTransformer <chris.g.noon@gmail.com>
2023-05-13 14:31:57 +0200

    adding remote backend base

--- aws/s3/terraform/simple/variables.tf
+++ aws/s3/terraform/simple/variables.tf
@@ -1,7 +1,7 @@
 # Input variable definitions
 
 variable "bucket_name" {
-  description = "name of the s3 bucket. Must be unique."
+  description = "name of the s3 bucket must be unique."
   type        = string
   default     = "CHANGE ME"
 }
