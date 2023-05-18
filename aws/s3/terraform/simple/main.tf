984f8a33a030aaa46732a151ec00089b833667af
UltTransformer <chris.g.noon@gmail.com>
2023-05-18 22:37:35 +0200

    testing tfc to env0 migration

--- aws/s3/terraform/simple/main.tf
+++ aws/s3/terraform/simple/main.tf
@@ -7,19 +7,19 @@ terraform {
     }
   }
   // Remove block
-  backend "remote" {
-    organization = "ChrisNoonPlayground"
-    workspaces {
-      name = "demo"
-    }
-  }
   # backend "remote" {
-  #   hostname     = "backend.api.env0.com"
-  #   organization = "a25b6a59-9c48-4917-b082-68e985319a87"
+  #   organization = "ChrisNoonPlayground"
   #   workspaces {
   #     name = "demo"
   #   }
   # }
+  backend "remote" {
+    hostname     = "backend.api.env0.com"
+    organization = "a25b6a59-9c48-4917-b082-68e985319a87"
+    workspaces {
+      name = "demo"
+    }
+  }
   // Remove block
 }
 
