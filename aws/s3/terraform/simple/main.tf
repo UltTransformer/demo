904a0c69179896d628ae3d482583d9881498df98
UltTransformer <chris.g.noon@gmail.com>
2023-05-18 22:41:57 +0200

    testing tfc to env0 migration

--- aws/s3/terraform/simple/main.tf
+++ aws/s3/terraform/simple/main.tf
@@ -13,13 +13,13 @@ terraform {
   #     name = "demo"
   #   }
   # }
-  backend "remote" {
-    hostname     = "backend.api.env0.com"
-    organization = "a25b6a59-9c48-4917-b082-68e985319a87"
-    workspaces {
-      name = "demo"
-    }
-  }
+  # backend "remote" {
+  #   hostname     = "backend.api.env0.com"
+  #   organization = "a25b6a59-9c48-4917-b082-68e985319a87"
+  #   workspaces {
+  #     name = "demo"
+  #   }
+  # }
   // Remove block
 }
 
