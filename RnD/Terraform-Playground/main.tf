3e77e927ab1943ad8d830c54006d3ef1ce29b175
UltTransformer <chris.g.noon@gmail.com>
2023-05-23 15:24:53 +0200

    adding module code

--- RnD/Terraform-Playground/main.tf
+++ RnD/Terraform-Playground/main.tf
@@ -1,4 +1,4 @@
 module "aws-ec2-module-simple" {
   source = "api.env0.com/a25b6a59-9c48-4917-b082-68e985319a87/aws-ec2-module-simple/awsec2modulesimple"
-  version = "1.0.0"
+  version = "1.0.1"
 }
