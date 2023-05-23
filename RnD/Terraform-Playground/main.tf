7e899bb656d98429a64a2164169d68b0016f3758
UltTransformer <chris.g.noon@gmail.com>
2023-05-23 15:28:07 +0200

    adding module code

--- RnD/Terraform-Playground/main.tf
+++ RnD/Terraform-Playground/main.tf
@@ -1,4 +1,20 @@
+# Module
+
 module "aws-ec2-module-simple" {
   source = "api.env0.com/a25b6a59-9c48-4917-b082-68e985319a87/aws-ec2-module-simple/awsec2modulesimple"
   version = "1.0.1"
+}
+
+# Provider
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
 }
