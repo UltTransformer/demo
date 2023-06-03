39a7f29c7befe21d9d6ccdfefa10c2fed9375820
Chris Noon <36542253+UltTransformer@users.noreply.github.com>
2023-06-03 14:50:35 +0200

    Create k3-install-single-node-part1.sh

---
+++ k3-install-single-node-part1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Recommended to run from sudo
+
+# Install HELM
+# https://helm.sh/docs/intro/install/
+cd /tmp
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# Install K3 & Cilium
+# https://docs.cilium.io/en/stable/installation/k8s-install-helm/
+
+# Add Cilium repo
+helm repo add cilium https://helm.cilium.io/
+
+# Install K3s without a CNI
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--flannel-backend=none --disable-network-policy' sh -
+
+# Reboot system
+reboot
