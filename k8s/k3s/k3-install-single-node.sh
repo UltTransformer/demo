e369877685ae880f26de25b710f81a84a657face
Chris Noon <36542253+UltTransformer@users.noreply.github.com>
2023-06-03 14:48:49 +0200

    Update k3-install-single-node.sh

--- k8s/k3s/k3-install-single-node.sh
+++ k8s/k3s/k3-install-single-node.sh
@@ -18,35 +18,3 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--flannel-backend=none --disabl
 
 # Reboot system
 reboot
-
-# Correct kubeconfig path
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-kubectl get pods --all-namespaces
-helm ls --all-namespaces
-
-# Install Cilium
-helm install cilium cilium/cilium --version 1.13.3 --set operator.replicas=1
-
-# Wait for nodes to turn to ready
-sleep 180
-
-# Restart all existing pods
-kubectl get pods --all-namespaces -o custom-columns=NAMESPACE:.metadata.namespace,NAME:.metadata.name,HOSTNETWORK:.spec.hostNetwork --no-headers=true | grep '<none>' | awk '{print "-n "$1" "$2}' | xargs -L 1 -r kubectl delete pod
-
-# Wait for the pods to restart
-sleep 180
-
-# Install Cilium CLI
-CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
-CLI_ARCH=amd64
-if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
-curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
-sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
-sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
-rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
-
-# Check Cilium status 
-cilium status --namespace=default
-
-# Run Cilium connectivity test
-cilium connectivity test --namespace=default
