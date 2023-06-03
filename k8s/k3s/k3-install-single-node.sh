8bcad839c8ec9cefe688ddecd9dfd15abf064558
Chris Noon <36542253+UltTransformer@users.noreply.github.com>
2023-06-03 02:03:10 +0200

    Update k3-install-single-node.sh

--- k8s/k3s/k3-install-single-node.sh
+++ k8s/k3s/k3-install-single-node.sh
@@ -1,1 +1,43 @@
+# Recommended to run from sudo
 
+# Install HELM
+# https://helm.sh/docs/intro/install/
+cd /tmp
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# Install K3 & Cilium
+# https://docs.cilium.io/en/stable/installation/k8s-install-helm/
+
+# Add Cilium repo
+helm repo add cilium https://helm.cilium.io/
+
+# Install K3s without a CNI
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--flannel-backend=none --disable-network-policy' sh -
+
+# Correct kubeconfig path
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+kubectl get pods --all-namespaces
+helm ls --all-namespaces
+
+# Install Cilium
+helm install cilium cilium/cilium --version 1.13.3 --namespace cilium
+
+# Restart all existing pods
+kubectl get pods --all-namespaces -o custom-columns=NAMESPACE:.metadata.namespace,NAME:.metadata.name,HOSTNETWORK:.spec.hostNetwork --no-headers=true | grep '<none>' | awk '{print "-n "$1" "$2}' | xargs -L 1 -r kubectl delete pod
+
+# Install Cilium CLI
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
+CLI_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+
+# Check Cilium status 
+cilium status --wait
+
+# Run Cilium connectivity test
+cilium connectivity test
