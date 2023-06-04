b3ae09e3ec1432b5806644fa7c1b354a7de234dd
Chris Noon <36542253+UltTransformer@users.noreply.github.com>
2023-06-04 19:27:43 +0200

    Update k3-install-single-node-part1.sh

--- k8s/k3s/k3-install-single-node-part1.sh
+++ k8s/k3s/k3-install-single-node-part1.sh
@@ -4,7 +4,7 @@
 
 # Install HELM
 # https://helm.sh/docs/intro/install/
-cd /tmp
+cd ~
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
