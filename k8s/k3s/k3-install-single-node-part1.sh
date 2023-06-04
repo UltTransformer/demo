765ccfb76787ffcd63c5b113b9da2e18c03d9b85
Chris Noon <36542253+UltTransformer@users.noreply.github.com>
2023-06-04 18:24:01 +0200

    Update k3-install-single-node-part1.sh

--- k8s/k3s/k3-install-single-node-part1.sh
+++ k8s/k3s/k3-install-single-node-part1.sh
@@ -18,5 +18,11 @@ helm repo add cilium https://helm.cilium.io/
 # Install K3s without a CNI
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--flannel-backend=none --disable-network-policy' sh -
 
+# Correct kubeconfig path
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+# Install Cilium
+helm install cilium cilium/cilium --version 1.13.3 --set operator.replicas=1
+
 # Reboot system
 reboot
