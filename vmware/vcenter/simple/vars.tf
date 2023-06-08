9de5b9ef0a055e534ecd865b17e0904728afd7b6
UltTransformer <chris.g.noon@gmail.com>
2023-06-08 17:00:10 +0200

    vmware vcenter base

---
+++ vmware/vcenter/simple/vars.tf
@@ -0,0 +1,14 @@
+variable "vsphere_user" {
+  type    = string
+  default = "username"
+}
+
+variable "vsphere_password" {
+  type    = string
+  default = "password"
+}
+
+variable "vsphere_server" {
+  type    = string
+  default = "server-ip"
+}
