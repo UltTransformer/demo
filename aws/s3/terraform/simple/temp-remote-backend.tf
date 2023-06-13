f56e65d3a71d48e4a207f7efc0998fe33861799b
UltTransformer <chris.g.noon@gmail.com>
2023-06-13 17:28:15 +0200

    update remote backend

--- aws/s3/terraform/simple/temp-remote-backend.tf
+++ aws/s3/terraform/simple/temp-remote-backend.tf
@@ -1,20 +1,20 @@
 # Remote backend
 
-terraform {
-  backend "remote" {
-    organization = "ChrisNoonPlayground"
-    workspaces {
-      name = "demo"
-    }
-  }
-}
-
 # terraform {
 #   backend "remote" {
-#     hostname = "backend.api.env0.com"
-#     organization = "a25b6a59-9c48-4917-b082-68e985319a87"
+#     organization = "ChrisNoonPlayground"
 #     workspaces {
 #       name = "demo"
 #     }
 #   }
 # }
+
+terraform {
+  backend "remote" {
+    hostname = "backend.api.env0.com"
+    organization = "a25b6a59-9c48-4917-b082-68e985319a87"
+    workspaces {
+      name = "demo"
+    }
+  }
+}
