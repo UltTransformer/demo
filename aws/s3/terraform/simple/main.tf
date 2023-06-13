9c2e705972b535edba097ec692e842a7fead9897
UltTransformer <chris.g.noon@gmail.com>
2023-06-13 11:49:44 +0200

    removing remote backend

--- aws/s3/terraform/simple/main.tf
+++ aws/s3/terraform/simple/main.tf
@@ -6,21 +6,6 @@ terraform {
       source = "hashicorp/aws"
     }
   }
-  // Remove block
-  # backend "remote" {
-  #   organization = "ChrisNoonPlayground"
-  #   workspaces {
-  #     name = "demo"
-  #   }
-  # }
-  # backend "remote" {
-  #   hostname     = "backend.api.env0.com"
-  #   organization = "a25b6a59-9c48-4917-b082-68e985319a87"
-  #   workspaces {
-  #     name = "demo"
-  #   }
-  # }
-  // Remove block
 }
 
 provider "aws" {
