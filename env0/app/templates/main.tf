b3e884cff1c085c6881bcc86c6a4194ce74aa83b
UltTransformer <chris.g.noon@gmail.com>
2023-06-14 19:09:26 +0200

    base TG build

---
+++ env0/app/templates/main.tf
@@ -0,0 +1,11 @@
+resource "env0_template" "example" {
+  name        = "example"
+  description = "Example template"
+  repository  = "https://github.com/env0/templates"
+  path        = "aws/hello-world"
+}
+
+resource "env0_template_project_assignment" "assignment" {
+  template_id = env0_template.example.id
+  project_id  = data.env0_project.default_project.id
+}
