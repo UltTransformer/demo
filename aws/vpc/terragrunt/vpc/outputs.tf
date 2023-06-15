30248df5a3fe649df030472908573ce124a28093
UltTransformer <chris.g.noon@gmail.com>
2023-06-15 20:20:24 +0200

    base TG build

--- aws/vpc/terragrunt/vpc/outputs.tf
+++ aws/vpc/terragrunt/vpc/outputs.tf
@@ -2,13 +2,17 @@
 
 output "vpc_1" {
   value = aws_vpc.vpc_1.id
+  default = "123456789"
 }
 output "subnet_1" {
   value = aws_subnet.subnet_1.id
+  default = "123456789"
 }
 output "subnet_2" {
   value = aws_subnet.subnet_2.id
+  default = "123456789"
 }
 output "subnet_3" {
   value = aws_subnet.subnet_3.id
+  default = "123456789"
 }
