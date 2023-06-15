626c9d0a9c0bdb3e5d8583c9732b95f678276527
UltTransformer <chris.g.noon@gmail.com>
2023-06-15 19:17:11 +0200

    base TG build

---
+++ aws/vpc/terragrunt/ec2/inputs.tf
@@ -0,0 +1,14 @@
+# Variable Imports
+
+data "aws_vpc" "vpc_1" {
+  id = data.terraform_remote_state.vpc.outputs.vpc_1
+}
+data "aws_subnet" "subnet_1" {
+  id = data.terraform_remote_state.vpc.outputs.subnet_1
+}
+data "aws_subnet" "subnet_2" {
+  id = data.terraform_remote_state.vpc.outputs.subnet_2
+}
+data "aws_subnet" "subnet_3" {
+  id = data.terraform_remote_state.vpc.outputs.subnet_3
+}
--- aws/vpc/terragrunt/ec2/variables.tf
+++ aws/vpc/terragrunt/ec2/variables.tf
@@ -1,36 +1,31 @@
 # Variables file
 
-variable "ec2_instance_size" {
-  type    = string
-  default = "t3.micro"
-}
-
-variable "ec2_name" {
-  type    = string
-  default = "vm1"
-}
-
 variable "region" {
   type    = string
   default = "eu-north-1a"
 }
 
+variable "ec2-instance-name" {
+  type    = string
+  default = "vm1"
+}
+
+variable "security-group-name" {
+  type    = string
+  default = "vm1"
+}
+
+variable "ec2_instance_size" {
+  type    = string
+  default = "t3.micro"
+}
+
 variable "ingress_sg_cidr" {
   type    = string
-  default = "10.0.0.0/16"
+  default = "10.10.0.0/16"
 }
 
 variable "egress_sg_cidr" {
   type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "security-group-name" {
-  type = string
-  default = "vm1"
-}
-
-variable "ec2-instance-name" {
-  type = string
-  default = "vm1"
+  default = "10.10.0.0/16"
 }
