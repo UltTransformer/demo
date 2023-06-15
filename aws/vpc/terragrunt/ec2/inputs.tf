cad57bcaee15b70dcee2651cd8f2066b5ef72205
UltTransformer <chris.g.noon@gmail.com>
2023-06-15 20:48:40 +0200

    base TG build

--- aws/vpc/terragrunt/ec2/inputs.tf
+++ aws/vpc/terragrunt/ec2/inputs.tf
@@ -1,11 +1,13 @@
 # Variable Imports
 
 data "aws_vpc" "vpc_1" {
+  default = "1234567890"
   tags = {
     Name   = "vpc_1"
   }
 }
 data "aws_subnet" "subnet_1" {
+  default = "1234567890"
   tags = {
     Name   = "subnet_1"
   }
