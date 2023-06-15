b648453a13fd5b34d822f333263f78a05ded0970
UltTransformer <chris.g.noon@gmail.com>
2023-06-15 19:44:03 +0200

    base TG build

--- aws/vpc/terragrunt/ec2/inputs.tf
+++ aws/vpc/terragrunt/ec2/inputs.tf
@@ -11,9 +11,9 @@ data "aws_subnet" "subnet_1" {
   }
 }
 
-data "aws_subnet" "subnet_2" {
-  id = data.terraform_remote_state.vpc.outputs.subnet_2
-}
-data "aws_subnet" "subnet_3" {
-  id = data.terraform_remote_state.vpc.outputs.subnet_3
-}
+# data "aws_subnet" "subnet_2" {
+#   id = data.terraform_remote_state.vpc.outputs.subnet_2
+# }
+# data "aws_subnet" "subnet_3" {
+#   id = data.terraform_remote_state.vpc.outputs.subnet_3
+# }
