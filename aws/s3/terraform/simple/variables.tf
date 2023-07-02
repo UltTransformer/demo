53446cef6c3cdab510ae1bc32e5b847a8b15b7aa
UltTransformer <chris.g.noon@gmail.com>
2023-07-02 11:51:07 +0300

    update s3 bucket name

--- aws/s3/terraform/simple/variables.tf
+++ aws/s3/terraform/simple/variables.tf
@@ -3,5 +3,5 @@
 variable "bucket_name" {
   description = "name of the s3 bucket must be unique."
   type        = string
-  default     = "2c9c29ecv2e9xb01x10wbx01wbx1w0bx"
+  default     = "111112ged9u2he0con23cib0r3nbf"
 }
