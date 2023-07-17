aee16c9d13526f2e78ea1422a6e3a4a3c59b5afa
UltTransformer <chris.g.noon@gmail.com>
2023-07-17 15:02:16 +0100

    env0 build start

--- env0/build/projects.tf
+++ env0/build/projects.tf
@@ -29,3 +29,27 @@ resource "env0_project" "EC2" {
   description = "EC2"
   parent_project_id = "8bcbf954-c080-453f-85a0-4502c3f0676e"
 }
+
+resource "env0_project" "S3" {
+  name        = "S3"
+  description = "S3"
+  parent_project_id = "a1fa2857-48f1-496a-8639-a2dcc36688ae"
+}
+
+resource "env0_project" "VPC" {
+  name        = "VPC"
+  description = "VPC"
+  parent_project_id = "a1fa2857-48f1-496a-8639-a2dcc36688ae"
+}
+
+resource "env0_project" "EC2" {
+  name        = "EC2"
+  description = "EC2"
+  parent_project_id = "a1fa2857-48f1-496a-8639-a2dcc36688ae"
+}
+
+resource "env0_project" "RnD" {
+  name        = "RnD"
+  description = "RnD"
+  parent_project_id = "a1fa2857-48f1-496a-8639-a2dcc36688ae"
+}
