f11ca147fdb62e2e7fc3738363d756fe4a73a59b
UltTransformer <chris.g.noon@gmail.com>
2023-07-17 10:17:03 +0100

    customer testing

--- RnD/Playground/main.tf
+++ RnD/Playground/main.tf
@@ -1,35 +1,32 @@
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
+    env0 = {
+      source = "env0/env0"
     }
   }
 }
-
-provider "aws" {
-  region     = "eu-west-1"
-}
-
 
 data "env0_configuration_variable" "firewalls" {
   name       = "firewalls"
   project_id = data.env0_project.tenant_env0_project.id
 }
 
+env0_tenant_project_variable = jsondecode(data.env0_configuration_variable.firewalls.value)
+### Pull data 
+data "env0_configuration_variable" "firewalls" {
+  name       = "firewalls"
+  project_id = data.env0_project.tenant_env0_project.id
+}
+
 locals {
+  # Convert the data to json
   env0_tenant_project_variable = jsondecode(data.env0_configuration_variable.firewalls.value)
+  # Build attempt 
   test = [
     {
       description = "test"
       ports       = [15672, 80]
-      services = [
-        "AMQP",
-        "AMQPS",
-        "HTTPS",
-        "STOMP",
-        "STOMPS",
-        "MQTT"
-      ]
+      services    = ["AMQP", "AMQPS", "HTTPS", "STOMP", "STOMPS", "MQTT"]
     }
   ]
 }
