0baedcecdfde09690b03385909a2be5e5d472d15
UltTransformer <chris.g.noon@gmail.com>
2023-07-17 11:07:40 +0100

    env0 build start

--- env0/build/projects.tf
+++ env0/build/projects.tf
@@ -1,9 +1,15 @@
 resource "env0_project" "build-production" {
-  name        = "build-production"
-  description = "build-production"
+  name        = "build-Production"
+  description = "build-Production"
 }
 
 resource "env0_project" "build-stage" {
-  name        = "build-stage"
-  description = "build-stage"
+  name        = "build-Stage"
+  description = "build-Stage"
+}
+
+resource "env0_project" "S3" {
+  name        = "S3"
+  description = "S3"
+  parent_project_id = data.env0_project.build-production.parent_project_id
 }
