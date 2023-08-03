938306b78de3fe0140928603ed90d10f67d017cc
UltTransformer <chris.g.noon@gmail.com>
2023-08-03 11:36:04 -0700

    adding env0 module call

--- aws/ec2/terraform/simple/main.tf
+++ aws/ec2/terraform/simple/main.tf
@@ -1,5 +1,5 @@
 # Provider
-# Customer Demo 7/3 17:32
+# Customer Demo ##/## ##:##
 
 terraform {
   required_providers {
@@ -13,64 +13,9 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-# Image(s)
+# Module call
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.20211001.1-x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["amazon"] # Canonical
+module "aws-ec2-module-simple" {
+  source = "api.env0.com/a25b6a59-9c48-4917-b082-68e985319a87/aws-ec2-module-simple/awsec2modulesimple"
+  version = "1.0.1"
 }
-
-# Security Group(s)
-
-resource "aws_security_group" "security_group" {
-  name   = var.security-group-name
-  vpc_id = data.aws_vpc.vpc_1.id
-
-  ingress {
-    from_port   = 0
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.ingress_sg_cidr]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [var.egress_sg_cidr]
-  }
-}
-
-# EC2
-
-# instance 1
-
-resource "aws_network_interface" "ec2_1" {
-  subnet_id = data.aws_subnet.subnet_1.id
-  security_groups = [
-    aws_security_group.security_group.id,
-  ]
-}
-
-resource "aws_instance" "ec2_1" {
-  ami               = data.aws_ami.amazon_linux.id
-  instance_type     = var.ec2_instance_size
-  availability_zone = var.region
-
-  network_interface {
-    network_interface_id = aws_network_interface.ec2_1.id
-    device_index         = 0
-  }
-  tags = {
-    Name = var.ec2-instance-name
-  }
-}
-
