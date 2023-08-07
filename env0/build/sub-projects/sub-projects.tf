107878e22c9ca619ef66bd0f35841edbe9e96332
UltTransformer <chris.g.noon@gmail.com>
2023-08-07 15:51:09 -0700

    adding sub-projects build

---
+++ env0/build/sub-projects/sub-projects.tf
@@ -0,0 +1,19 @@
+# Sub Projects
+
+resource "env0_project" "S3" {
+  name        = "S3"
+  description = "S3"
+  parent_project_id = "8bcbf954-c080-453f-85a0-4502c3f0676e"
+}
+
+resource "env0_project" "VPC" {
+  name        = "VPC"
+  description = "VPC"
+  parent_project_id = "8bcbf954-c080-453f-85a0-4502c3f0676e"
+}
+
+resource "env0_project" "EC2" {
+  name        = "EC2"
+  description = "EC2"
+  parent_project_id = "8bcbf954-c080-453f-85a0-4502c3f0676e"
+}
