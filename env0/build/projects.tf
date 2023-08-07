5b18ec7c7bde807002c403eef3f979d50732c585
UltTransformer <chris.g.noon@gmail.com>
2023-08-07 15:59:12 -0700

    removing root projects

--- env0/build/projects.tf
+++ env0/build/projects.tf
@@ -1,11 +1,11 @@
-# Root projects
+# # Root projects
 
-resource "env0_project" "build-production" {
-  name        = "build-Production"
-  description = "build-Production"
-}
+# resource "env0_project" "build-production" {
+#   name        = "build-Production"
+#   description = "build-Production"
+# }
 
-resource "env0_project" "build-stage" {
-  name        = "build-Stage"
-  description = "build-Stage"
-}
+# resource "env0_project" "build-stage" {
+#   name        = "build-Stage"
+#   description = "build-Stage"
+# }
