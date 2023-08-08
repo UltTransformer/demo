7f304554a24915ac126fae83051820dd19800504
UltTransformer <chris.g.noon@gmail.com>
2023-08-08 08:47:37 -0700

    adding aws key pull from vault

--- aws/s3/terraform/simple/main.tf
+++ aws/s3/terraform/simple/main.tf
@@ -5,6 +5,8 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      access_key = data.aws_secretsmanager_id_version.current.secret_binary
+      secret_key = data.aws_secretsmanager_secret_version.current.secret_binary
     }
   }
   backend "remote" {  
@@ -30,4 +32,21 @@ resource "aws_s3_bucket_versioning" "s3_bucket" {
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+# Pull secrets
+# Key
+data "aws_secretsmanager_id" "AWS_ACCESS_KEY_ID" {
+  arn = "arn:aws:secretsmanager:eu-north-1:176620772543:secret:AWS_ACCESS_KEY_ID-zD07gd"
+}
+data "aws_secretsmanager_id_version" "current" {
+  secret_id = data.aws_secretsmanager_id.AWS_ACCESS_KEY_ID.id
+}
+
+# Secret
+data "aws_secretsmanager_secret" "AWS_SECRET_ACCESS_KEY" {
+  arn = "arn:aws:secretsmanager:eu-north-1:176620772543:secret:AWS_SECRET_ACCESS_KEY-zD07gd"
+}
+data "aws_secretsmanager_secret_version" "current" {
+  secret_id = data.aws_secretsmanager_secret.AWS_SECRET_ACCESS_KEY.id
 }
