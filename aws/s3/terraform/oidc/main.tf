e72ca0f49f28a102ae6d2dec434b9c04ef9ffb74
UltTransformer <chris.g.noon@gmail.com>
2023-08-09 11:18:06 -0700

    creating oidc s3 bucket

---
+++ aws/s3/terraform/oidc/main.tf
@@ -0,0 +1,34 @@
+# Customer change 8/4 07:42
+
+# Provider
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "remote" {  
+  organization = "ChrisNoonPlayground"
+  workspaces {
+    name = "demo"
+    }
+}
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+# Bucket
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_versioning" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
---
+++ aws/s3/terraform/oidc/variables.tf
@@ -0,0 +1,7 @@
+# Input variable definitions
+
+variable "bucket_name" {
+  description = "name of the s3 bucket must be unique."
+  type        = string
+  default     = "111112ged9u2he0con23cib0r3nbf"
+}
