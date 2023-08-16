038b37e182cb102674db84e82a2d4b956b83a385
UltTransformer <chris.g.noon@gmail.com>
2023-08-16 15:53:55 -0700

    updating remote backend

--- aws/vpc/terraform/simple/remote-backend.tf
+++ aws/vpc/terraform/simple/remote-backend.tf
@@ -11,7 +11,7 @@
 terraform {
   backend "remote" {
     hostname = "backend.api.env0.com"
-    organization = "a25b6a59-9c48-4917-b082-68e985319a87.7d4afe47-cc34-4d6e-86df-fe030c699a51"
+    organization = "a25b6a59-9c48-4917-b082-68e985319a87.ba6173ab-0637-4108-a6d7-2c3c414ad727"
     workspaces {
       name = "demo-vpc"
     }
