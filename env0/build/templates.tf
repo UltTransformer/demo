a4517700f450e657e05e4db00bd06267b293f883
UltTransformer <chris.g.noon@gmail.com>
2023-10-23 11:30:50 -0400

    adding template code

--- env0/build/templates.tf
+++ env0/build/templates.tf
@@ -2,6 +2,8 @@ resource "env0_template" "build-template" {
   name                   = "build-template"
   description            = "TF built template"
   repository             = "https://github.com/UltTransformer/demo"
+  revision               = "main"
+  path                   = "folder1/folder2/folder3"
   terraform_version      = "1.1.9"
   github_installation_id = 27876397
 }
